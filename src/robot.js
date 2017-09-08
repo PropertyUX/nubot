a2af6b2e828a22724463116b3b1544d41bc8c4ee
timkinnane <timkinnane@gmail.com>
2017-09-08 12:00:33 +1000

    chore(robot): Remove coffee require that was duplicated in index

--- src/robot.js
+++ src/robot.js
@@ -3,7 +3,6 @@
 const EventEmitter = require('events').EventEmitter
 const fs = require('fs')
 const path = require('path')
-require('coffee-script') // registers extension for legacy script loading
 
 const async = require('async')
 const Log = require('log')
