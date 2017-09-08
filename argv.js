fee95e0342c6d2380e7d8db14224be1d058cb1c1
timkinnane <timkinnane@gmail.com>
2017-09-08 11:03:10 +1000

    feat(argv): Let index.js use cli args when run by node
    Also adds support for env args, package.json config object and fix compatibility with conflicting
    httpd and disable-httpd args.

---
+++ argv.js
@@ -0,0 +1,72 @@
+'use strict'
+
+// Pass command line args and env vars to produce options object
+
+const yargs = require('yargs')
+
+const getArgv = (robot) => yargs
+  .usage('\nUsage:                      $0 [args]')
+  .env('HUBOT')
+  .pkgConf('botConfig')
+  .option('adapter', {
+    alias: 'a',
+    type: 'string',
+    describe: 'The adapter name to use.\nIf its not a default adapter it will require from node modules with "hubot-" prefix.\n',
+    default: 'shell'
+  })
+  .example('adapter', 'bin/nubot -a rocketchat (will require "hubot-rocketchat")')
+  .option('httpd', {
+    type: 'boolean',
+    describe: 'Enable the HTTP server.\nThis option added to keep cli args consistent with internal options.\n',
+    default: true
+  })
+  .option('disable-httpd', {
+    alias: 'd',
+    type: 'boolean',
+    describe: 'Disiable the HTTP server.\nFor compatibility with hubot v2 args, overrides `http` if both set.\n',
+    default: false
+  })
+  .option('name', {
+    alias: 'n',
+    type: 'string',
+    describe: 'Name of the robot in chat.\nPrepending any command with the robot\'s name will trigger respond listeners.\n',
+    default: 'nubot'
+  })
+  .option('alias', {
+    alias: 'l',
+    type: 'string',
+    describe: 'Alternate name for robot.\n',
+    default: false
+  })
+  .option('scripts', {
+    alias: ['require', 'r'],
+    type: 'array',
+    describe: 'Alternative scripts paths\n',
+    default: []
+  })
+  .config()
+  .alias('config', 'c')
+  .example('config', 'bin/nubot -c bot-config.json')
+  .version(() => require('./package.json').version)
+  .alias('version', 'v')
+  .help()
+  .alias('help', 'h')
+  .epilogue('Any option can be provided as an environment variable, with the prefix HUBOT_\nConfig can also be declared in package.json with the key: "botConfig"\nFor more information, see https://propertyux.github.io/nubot')
+  .check((yargs) => {
+    if (yargs.disableHttpd) yargs.httpd = !yargs.disableHttpd
+    return yargs
+  })
+  .fail(function (msg, err) {
+    console.error('ERROR: ' + msg)
+    console.error('Start with --help for config argument info.')
+    if (err) throw err // preserve stack
+    process.exit(1)
+  })
+  .argv
+
+// TODO - re-instate `check` as yargs command to dry-run config
+
+if (process.platform !== 'win32') process.on('SIGTERM', () => process.exit(0))
+
+// call on require to just returns the options
+module.exports = getArgv()
--- index.js
+++ index.js
@@ -7,26 +7,20 @@ if (process.env.NOVE_ENV === 'development') {
 require('coffee-script').register()
 const fs = require('fs')
 const {resolve} = require('path')
+const config = require('./argv')
 const Nubot = require('./src')
 const mockery = require('mockery')
 const playbook = require('hubot-playbook')
 mockery.enable({ warnOnReplace: false, warnOnUnregistered: false })
 mockery.registerSubstitute('hubot', 'nubot')
 
-const defaults = {
-  adapter: process.env.NUBOT_ADAPTER || process.env.HUBOT_ADAPTER || 'shell',
-  alias: process.env.NUBOT_ALIAS || process.env.HUBOT_ALIAS || false,
-  create: process.env.NUBOT_CREATE || process.env.HUBOT_CREATE || false,
-  disableHttpd: !process.env.NUBOT_HTTPD || !process.env.HUBOT_HTTPD || false,
-  scripts: process.env.NUBOT_SCRIPTS || process.env.HUBOT_SCRIPTS || [],
-  name: process.env.NUBOT_NAME || process.env.HUBOT_NAME || 'Nubot',
-  path: process.env.NUBOT_PATH || process.env.HUBOT_PATH || '.',
-  configCheck: false
-}
-
-Nubot.start = function (options) {
-  let config = Object.assign(defaults, options)
-  const robot = Nubot.loadBot(undefined, config.adapter, !config.disableHttpd, config.name, config.alias)
+Nubot.start = function () {
+  const robot = Nubot.loadBot(
+    config.adapter,
+    config.enableHttpd,
+    config.name,
+    config.alias
+  )
   robot.adapter.on('connected', () => loadScripts(robot, config.scripts))
   robot.run()
   return robot
@@ -57,4 +51,4 @@ function loadExternalScripts (robot) {
   })
 }
 
-module.exports = Nubot
+Nubot.start()
