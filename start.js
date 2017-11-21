0a7abebc73afcc698697920c470621ef58c20141
timkinnane <timkinnane@gmail.com>
2017-11-22 00:05:00 +1100

    fix(env): Use dotenv to process .env file on start

--- start.js
+++ start.js
@@ -1,7 +1,4 @@
 'use strict'
 
-// example running robot without bin, e.g. to use with node debug
-// start method accepts options that override config args
-
-const nubot = require('./')
-nubot.start({ adapter: 'shell' })
+require('dotenv').config()
+require('./').start()
